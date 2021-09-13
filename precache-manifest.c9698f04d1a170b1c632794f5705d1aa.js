cf571eae405bd9e87debfb34e15cd6b868b110fe
Val Lipat <vlipat.dev@gmail.com>
2021-09-13 17:57:48 +0800

    Updates

---
+++ precache-manifest.c9698f04d1a170b1c632794f5705d1aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85000a5f623c28ba98f25036c6168792",
+    "url": "/frontend-resources/index.html"
+  },
+  {
+    "revision": "e06e496308bd74d07092",
+    "url": "/frontend-resources/static/css/main.04cc4e91.chunk.css"
+  },
+  {
+    "revision": "34230216794e94cb2b4b",
+    "url": "/frontend-resources/static/js/2.3dd36fe7.chunk.js"
+  },
+  {
+    "revision": "bf71ac297fcfe9a3d2d199a48fc3f60c",
+    "url": "/frontend-resources/static/js/2.3dd36fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06e496308bd74d07092",
+    "url": "/frontend-resources/static/js/main.216b15fd.chunk.js"
+  },
+  {
+    "revision": "4c3060f4d340c68e8b7f",
+    "url": "/frontend-resources/static/js/runtime-main.596e8ca1.js"
+  },
+  {
+    "revision": "adf4518bd2d137cb10deb3b563c2d6d9",
+    "url": "/frontend-resources/static/media/undraw-designer.adf4518b.png"
+  }
+]);
